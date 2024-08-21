889b534a37c958cef3f4c17819ae3443ac07358e
JusteKal <66133903+JusteKal@users.noreply.github.com>
2024-08-21 19:42:54 +0200

    .

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
         burgerMenu.classList.toggle('active');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const fileInput = document.getElementById('upload');
+    const uploadLabel = document.getElementById('uploadlabel');
+
+    fileInput.addEventListener('change', function () {
+        if (fileInput.files.length > 0) {
+            uploadLabel.classList.add('file-selected');
+            uploadLabel.classList.remove('file-not-selected');
+            uploadLabel.textContent = ('Fichier sélectionné');
+        } else {
+            uploadLabel.classList.add('file-not-selected');
+            uploadLabel.classList.remove('file-selected');
+            uploadLabel.textContent = 'Choisir un fichier';
+        }
+    });
+});
