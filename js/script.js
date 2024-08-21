a423b947f3c2fc16476eca0f8d713a524d44cec9
JusteKal <66133903+JusteKal@users.noreply.github.com>
2024-08-21 17:53:24 +0200

    Ajout du menu hamburger

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.getElementById('burgerMenu');
+    const navLinks = document.getElementById('navLinks');
+
+    burgerMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        burgerMenu.classList.toggle('active');
+    });
+});
