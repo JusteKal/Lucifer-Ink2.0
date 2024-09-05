9648da7d745b910a0c260c4f7239681179c6ba75
JusteKal <66133903+JusteKal@users.noreply.github.com>
2024-09-05 20:26:53 +0200

    + animation pour la page d'accueil

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burgerMenu = document.getElementById('burgerMenu');
     const navLinks = document.getElementById('navLinks');
+    const images = document.querySelectorAll('.div1, .div2, .div3, .div4, .div5, .div6');
 
     burgerMenu.addEventListener('click', () => {
         navLinks.classList.toggle('active');
         burgerMenu.classList.toggle('active');
     });
+
+    const revealImages = () => {
+        images.forEach(image => {
+            const imageTop = image.getBoundingClientRect().top;
+            const imageHeight = image.offsetHeight;
+            const windowHeight = window.innerHeight;
+            if (imageTop < windowHeight && imageTop + imageHeight > 0) {
+                image.classList.add('visible');
+            }
+        });
+    };
+
+    revealImages(); // Call revealImages initially to reveal images on page load
 });
